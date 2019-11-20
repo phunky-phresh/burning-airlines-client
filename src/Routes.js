1783fb600504425cf6ccee944349a78804492554
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-20 12:05:22 +1100

    added routes

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Home from './components/Home'
+import Search from './components/Search'
+import Flights from './components/Flights'
+import Booking from './components/Booking'
+import Planes from './components/Planes'
+
+const Routes = (
+  <Router>
+    <div>
+      <Route exact path="/" component={ Home } />
+      <switch>
+        <Route path='/:user/Search' component={ Search } />
+        <Route path='/:user/Flights' component={ Flights } />
+        <Route path='/:user/Flights/:flight' component={ Booking } />
+        <Route path='/:user/Planes' component={ Planes } />
+        <Redirect from='/:user' to='/:user/Search' />
+      </switch>
+    </div>
+  </Router>
+);
+
+export default Routes;
