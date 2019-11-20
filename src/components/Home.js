03c78c454cb054afb81bfb34440914b42744623d
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-20 12:06:21 +1100

    initialise js files

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page coming soon</h1>
+    </div>
+  )
+};
+
+export default Home;
